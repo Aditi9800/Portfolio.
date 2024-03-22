52290db501925acf088c78f24f56ff5744c26ac8
Aditi Sahu <aditisahu9800@gmail.com>
2024-03-22 16:08:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -22,3 +22,18 @@ window.onscroll=()=>{
     let header=document.querySelector('.header');
     header.classList.toggle('sticky',window.scrollY>100);
     };
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less"; 
+    moreText.style.display = "inline";
+  }
+}
